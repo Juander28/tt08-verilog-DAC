dc0e5463cfc34cecc6f506d068d6fd6e3b42ca4e
Juander28 <85529742+Juander28@users.noreply.github.com>
2024-09-03 17:46:34 -0500

    Add files via upload

--- src/CONTROL.v
+++ src/CONTROL.v
@@ -21,9 +21,9 @@
 
 
 module CONTROL(
-					 input wire in_vref, in_data, in_conver, in_comp, clk,  rst,
-					 output [2:0] VR, VM, VRC
-					 );
+					 input wire in_vref, in_data, in_conver, in_comp, clk,  rst, stop, temp,
+					 output [2:0] VR, VM, VRC);
+					 
 			 wire stp_vref, stp_data, stp_conver, stp_comp;
 			 wire [3:0]vref;
 			 wire [7:0]data;
@@ -56,6 +56,25 @@ module CONTROL(
 						 .en(!stp_comp),
 						 .d(in_comp),
 						 .buffer_s({comp,stp_comp}));	 
+						 
+			d_ff low [2:0](
+								.clk(clk),
+								.reset(rst),
+								.en(stop),
+								.d({temp,temp, temp}),
+								.q(VR));
+			d_ff med [2:0](
+								.clk(clk),
+								.reset(rst),
+								.en(stop),
+								.d({temp,temp, temp}),
+								.q(VM));
+			d_ff high [2:0](
+								.clk(clk),
+								.reset(rst),
+								.en(stop),
+								.d({temp,temp, temp}),
+								.q(VRC));
 
 endmodule
 
